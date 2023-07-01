806ac1a0bf21fe0a2424bb5b68c6e69a2aca6d7d
Ritesh Kr <ritesh123kumar081@gmail.com>
2023-07-01 17:52:51 +0530

    initial Commit

---
+++ University-Event-Management/src/test/java/com/ritesh/University/Event/Management/UniversityEventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritesh.University.Event.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityEventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
